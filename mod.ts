b9844072cb18aae1c3a31058f1018e1f6f7c0841
Andrew C. H. McMillan <30243134+Summertime@users.noreply.github.com>
2023-12-16 20:27:23 +1030

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,28 @@
+export const middlestack = (handlers) => async (req: Request) => {
+    return await handlers.reduceRight(
+        (prior, current) => current(prior),
+        (req: Request) => new Response('404', { status: 404 }),
+    )(req);
+};
+
+export const set_header = (key, value) => (next) => async (req: Request) => {
+    const res = await next(req);
+    res.headers.set(key, value);
+    return res;
+};
+
+export const error_handler = () => (next) => async (req: Request) => {
+    try {
+        return await next(req);
+    } catch {
+        return new Response('Error', { status: 500 });
+    }
+};
+
+const route = (method, path, code) => (next) => async (req: Request) => {
+    const pattern = new URLPattern(path, req.url);
+    const match = pattern.exec(req.url);
+    if (req.method === method && match !== null)
+        return await code(req, match);
+    return await next(req);
+};
